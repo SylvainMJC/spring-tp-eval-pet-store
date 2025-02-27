30ed54a05c1cbf81b26b3e5bd6dcee6197ba468d
Sylvain Conan <sylvain.mj.conan@gmail.com>
2025-02-27 14:49:30 +0100

    first commit

---
+++ src/main/java/fr/epsi/b3devc1/sylvainmjc/springtpevalpetstore/SpringTpEvalPetStoreApplication.java
@@ -0,0 +1,13 @@
+package fr.epsi.b3devc1.sylvainmjc.springtpevalpetstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTpEvalPetStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTpEvalPetStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/epsi/b3devc1/sylvainmjc/springtpevalpetstore/SpringTpEvalPetStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epsi.b3devc1.sylvainmjc.springtpevalpetstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTpEvalPetStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
